c45f6529f9528ed29b6cef7f87be65d5e75b371b
Misha <64092567+misha-pyshark@users.noreply.github.com>
2020-05-31 00:12:40 -0400

    Create email-sender.py

---
+++ email-sender.py
@@ -0,0 +1,24 @@
+import smtplib
+import smtplib
+from email_credentials import *
+
+def send_email(subject, body, from_email, password, to_email):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(from_email, password)
+        message = f'Subject: {subject}\n\n{body}'
+        server.sendmail(from_email, to_email, message)
+        server.quit()
+        print('Email sent successfully')
+    except:
+        print("Email couldn't be sent")
+        pass
+
+
+if __name__=='__main__':
+    recipient = input("Please enter recepient's email: ")
+    s = input('Please enter the subject of the email: ')
+    b = input('Please enter the content of the email: ')
+    send_email(s,b, myemail, mypassword, recipient)
